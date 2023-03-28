78c3f17971bf98218b741baca67a323457b7cdb6
Madison Lester <madiali@ad.unc.edu>
2023-03-28 19:25:23 -0400

    Minor fix

--- drivers/net/ethernet/intel/e1000.c
+++ drivers/net/ethernet/intel/e1000.c
@@ -13,5 +13,5 @@ static void __exit e1000_exit_module(void)
     pr_info("Hello from e1000.c hello mod init\n");
 }
 
-module_init(hello_mod_init);
-module_exit(hello_mod_exit);
+module_init(e1000_init_module);
+module_exit(e1000_exit_module);
